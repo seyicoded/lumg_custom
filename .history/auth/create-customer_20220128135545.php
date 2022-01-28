c9f672605d0c9f25de2610a0babfcf06fd77844d
Opadonu Seyi <opadonuseyi01@gmail.com>
2022-01-28 13:58:23 +0100

    changed2

--- .history/auth/create-customer_20220128135545.php
+++ .history/auth/create-customer_20220128135545.php
@@ -4,5 +4,7 @@
     $config = new config();
     $con = $config->database();
 
+    require '../security.php';
+
     echo mysqli_num_rows(mysqli_query($con, "SELECT * FROM wpdc_users"));
 ?>
---
+++ security.php
@@ -0,0 +1,20 @@
+<?php
+    $consumer_key = $_REQUEST['consumer_key'];
+    $consumer_secret = $_REQUEST['consumer_key'];
+
+    if($consumer_key != 'ck_b2c5927c3fea3a23cf914e01bf804e752e0c0db4' && $consumer_secret != 'cs_49b054091a69141a09f042b56ad40bb467205dd5'){
+        return json_encode(
+            [
+                'status' => 'false',
+                'message' => 'consumer information is invalid'
+            ]
+        );
+
+        die(json_encode(
+            [
+                'status' => 'false',
+                'message' => 'consumer information is invalid'
+            ]
+        ));
+    }
+?>
